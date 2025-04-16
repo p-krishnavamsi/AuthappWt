704ace4b6a8e274fe52cd5be67cbf9e27ac483e1
P krishna Vamsi <krishnavamsi7215@gmail.com>
2025-04-16 11:44:32 +0530

    env

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://ganesh:ganesh123@cluster0.vbk5oaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
 .catch(err => console.log("DB Error:", err));
